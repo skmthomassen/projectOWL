e67d75aff689a3168e82597493426f29de676eed
skmthomassen <kim@aptget.dk>
2017-08-25 15:08:51 +0200

    fixet dl script so it segments the video files into multiple files

--- record/recordstreams.sh
+++ record/recordstreams.sh
@@ -19,13 +19,15 @@ for ((n=0;n<2;n++))
 do
   l="$(($n+1))"
   IP=$(sed "$l!d" $PWD/camIPs)
-  at now +1 minutes <<< "ffmpeg -loglevel error -thread_queue_size 512 -rtsp_transport tcp -i rtsp://192.168.0.$IP/av0_0 -y -c:v copy -t $TIME "$PWD/clips/$SUF-$IP.ts"" &
+  at now +1 minutes <<< "ffmpeg -loglevel error -thread_queue_size 512 \
+    -rtsp_transport tcp -i rtsp://192.168.0.$IP/av0_0 -f segment -segment_time 4 \
+    -y -c:v copy -segment_format mpegts -t $TIME "$PWD/clips/$SUF-$IP-%05d.ts"" &
 done
 
-at now minutes <<< "ffmpeg -thread_queue_size 512 -f alsa -i hw:2 -y -acodec copy -t $TIME "$PWD/clips/$SUF-audio.wav"" &
+#at now minutes <<< "ffmpeg -thread_queue_size 512 -f alsa -i hw:2 -y -acodec copy -t $TIME "$PWD/clips/$SUF-audio.wav"" &
 
-END=$(date +%s)
-TOTALTIME=$(expr $END - $BEGIN)
-echo "Total time of execution: " $TOTALTIME"s || "$(($TOTALTIME/60))"m || "$(($TOTALTIME/3600))"h"
+#END=$(date +%s)
+#TOTALTIME=$(expr $END - $BEGIN)
+#echo "Total time of execution: " $TOTALTIME"s || "$(($TOTALTIME/60))"m || "$(($TOTALTIME/3600))"h"
 
 exit 0
