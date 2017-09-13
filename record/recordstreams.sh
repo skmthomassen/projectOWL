65e0f7715bdb0e620d14f513e5e620aec0244cb7
skmthomassen <kim@aptget.dk>
2017-09-13 15:44:29 +0200

    print suffux at runtime end

--- record/recordstreams.sh
+++ record/recordstreams.sh
@@ -6,7 +6,7 @@ if [ $# -ne 1 ] ; then
 fi
 
 TIME=$1
-SEGTIME="300"
+SEGTIME="600"
 SECONDS=0
 IP0="192.168.0.200"
 IP1="192.168.0.201"
@@ -41,14 +41,15 @@ echo
 
 parallel --progress --verbose --joblog $PWD/logs/$SUF.log ::: \
 "ffmpeg -hide_banner -thread_queue_size 512 -rtsp_transport tcp -i rtsp://$IP2/av0_0 -f segment \
-  -segment_time $SEGTIME -y -c:v copy -segment_format mpegts -t 15 "$PWD/clips/$SUF-202-%05d.ts"" \
+  -segment_time $SEGTIME -y -c:v copy -segment_format mpegts -t 15 "$PWD/clips/$SUF-202-%03d.ts"" \
 "ffmpeg -hide_banner -thread_queue_size 512 -rtsp_transport tcp -i rtsp://$IP3/av0_0 -f segment \
-  -segment_time $SEGTIME -y -c:v copy -segment_format mpegts -t 15 "$PWD/clips/$SUF-203-%05d.ts"" \
+  -segment_time $SEGTIME -y -c:v copy -segment_format mpegts -t 15 "$PWD/clips/$SUF-203-%03d.ts"" \
 "ffmpeg -hide_banner -thread_queue_size 512 -f alsa -i hw:1 -y -f segment \
-  -segment_time $SEGTIME -segment_format aac -acodec aac -t $TIME "$PWD/clips/$SUF-audio-%05d.aac""
+  -segment_time $SEGTIME -segment_format aac -acodec aac -t $TIME "$PWD/clips/$SUF-audio-%03d.aac""
 
 
 DUR=$SECONDS
 echo "Actual time recorded:" $(($DUR/86400))" days "$(date -d "1970-01-01 + $DUR seconds" "+%H hours %M minutes %S seconds")
+echo "---Ending recording job no: $SUF---"
 
 exit 0
