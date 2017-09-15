ddeceb6245793138a889fb057e40b74ff925bbbe
skmthomassen <kim@aptget.dk>
2017-09-15 13:30:01 +0200

    using ffplay to play audio

--- soundBEEP/beeper.py
+++ soundBEEP/beeper.py
@@ -2,23 +2,34 @@
 
 from pygame import mixer # Load the required library
 import time
+import subprocess
 
 #
 #LOADING THE SOUND THING
 #
-mixer.init()
-mixer.music.load('beep005.mp3')
+#mixer.init()
+#mixer.music.load('beep005.mp3')
+filetoplay = '/home/kim/projectOWL/soundBEEP/beep005.mp3'
+
+def play(audio_file):
+    subprocess.call(["ffplay", "-hide_banner", "-loglevel", "-8", "-nodisp", "-autoexit", filetoplay])
 
 #
 #OTHER LOOP TRY
 #
-n = 5
+interval = 10
 starttime=time.time()
 print ("Starting beep job...")
 count = 0
 while True:
-  mixer.music.play()
-  print ("beep...", count)
-  time.sleep(n - ((time.time() - starttime) % n))
+  if count == 11:
+    count = 0
+  repeat = 0
+  while repeat < count:
+      #print ("repeat...", repeat)
+      play(filetoplay)
+      repeat += 1
+  #print ("count...", count)
+  time.sleep(interval - ((time.time() - starttime) % interval))
   count += 1
 #
