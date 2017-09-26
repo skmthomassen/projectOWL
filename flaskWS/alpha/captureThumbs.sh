4820226a850ddeadd6c55b1edf5e793807992bd2
skmthomassen <kim@aptget.dk>
2017-09-26 09:57:33 +0200

    ws - added PID files, changed thumbsCature to utilizing small image output

--- flaskWS/alpha/captureThumbs.sh
+++ flaskWS/alpha/captureThumbs.sh
@@ -1,26 +1,12 @@
 #/bin/bash
 
-#FFMPEG202 = "ffmpeg -y -hide_banner -loglevel -8 -i rtsp://192.168.0.202 -frames 1 202left.png"
-#FFMPEG203 = "ffmpeg -y -hide_banner -loglevel -8 -i rtsp://192.168.0.203 -frames 1 203right.png"
-
-#echo "MINE: " $$
-
 while true
 do
   parallel ::: \
-    "ffmpeg -y -hide_banner -loglevel -8 -i rtsp://192.168.0.202 -frames 1 $PWD/static/thumbs/202left.png" \
-    "ffmpeg -y -hide_banner -loglevel -8 -i rtsp://192.168.0.203 -frames 1 $PWD/static/thumbs/203right.png"
+    "ffmpeg -y -hide_banner -loglevel -8 -i rtsp://192.168.0.202/av0_1 -frames 1 $PWD/static/thumbs/202left.png" \
+    "ffmpeg -y -hide_banner -loglevel -8 -i rtsp://192.168.0.203/av0_1 -frames 1 $PWD/static/thumbs/203right.png"
     sleep 3
 
 done
-
-
-
-#myprogram &
-#echo "$!"  > /tmp/pid
-
-#myCommand & echo $!
-
-
 
 exit 0
