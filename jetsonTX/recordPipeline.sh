9db3ca1c8905d3935613bdf7266b7691eafb454b
skmthomassen <kim@aptget.dk>
2017-11-13 15:49:45 +0100

    updated gst pipeline - VA w smooth playback

--- jetsonTX/recordPipeline.sh
+++ jetsonTX/recordPipeline.sh
@@ -11,10 +11,10 @@ echo "Starting recoring: "$FILENAME
 
 gst-launch-1.0 -e \
 	v4l2src \
-		! progressreport update-freq=10 \
 		! queue max-size-buffers=0 max-size-time=0 max-size-bytes=0 \
-		! 'video/x-raw, format=(string)I420, width=(int)3840, height=(int)2160' \
-		! videoconvert \
+		! "video/x-raw, format=(string)UYVY, framerate=30/1, width=(int)3840, height=(int)2160" \
+		! nvvidconv \
+		! "video/x-raw(memory:NVMM), format=(string)I420, framerate=30/1, width=(int)3840, height=(int)2160" \
 		! omxh265enc \
 		! mux. \
 	alsasrc device=$A_DEV \
@@ -27,3 +27,32 @@ gst-launch-1.0 -e \
 
 
 exit 0
+
+#gst-launch-1.0 v4l2src ! queue max-size-buffers=0 max-size-time=0 max-size-bytes=0 ! "video/x-raw, format=(string)UYVY, framerate=30/1, width=(int)3840, height=(int)2160" ! nvvidconv ! "video/x-raw(memory:NVMM), format=(string)I420, framerate=30/1, width=(int)3840, height=(int)2160" ! omxh264enc ! matroskamux ! queue ! \filesink location=now1.mkv
+
+#gst-launch-1.0 
+#	v4l2src 
+#		! "video/x-raw, format=(string)UYVY, width=(int)3840, height=(int)2160" 
+#		! nvvidconv 
+#			! "video/x-raw(memory:NVMM), format=(string)I420, width=(int)3840, height=(int)2160" 
+#		! omxh264enc 
+#		! matroskamux 
+#		! queue 
+#	! filesink location=file.mkv
+
+#gst-launch-1.0 -e \
+#	v4l2src \
+#		! progressreport update-freq=10 \
+#		! queue max-size-buffers=0 max-size-time=0 max-size-bytes=0 \
+#		! 'video/x-raw, format=(string)I420, framerate=30/1, width=(int)3840, height=(int)2160' \
+#		! videoconvert \
+#		! omxh265enc \
+#		! mux. \
+#	alsasrc device=$A_DEV \
+#		! queue max-size-buffers=0 max-size-time=0 max-size-bytes=0 \
+#		! audioconvert \
+#		! flacenc \
+#		! mux. \
+#	matroskamux name=mux min-index-interval=1000000000\
+#		! filesink location=$VID_DIR/$FILENAME.mkv
+
