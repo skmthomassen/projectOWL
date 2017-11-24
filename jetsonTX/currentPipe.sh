7d6e29d98d40d90a333923f0b700a7758a6a91fc
skmthomassen <kim@aptget.dk>
2017-11-24 13:44:40 +0100

    cur

--- jetsonTX/currentPipe.sh
+++ jetsonTX/currentPipe.sh
@@ -15,14 +15,29 @@ mkdir -p $FILE_PATH
 echo "Starting recoring: " $FILE_PATH/$DAYANDTIME
 
 case $ARG in
-	-4)
+	-5)
 	GST_DEBUG=3 gst-launch-1.0 -e nvcamerasrc fpsRange="30 30" \
-		! "video/x-raw(memory:NVMM), width=(int)1920, height=(int)1080, format=(string)I420, framerate=(fraction)30/1" \
+		! "video/x-raw(memory:NVMM), width=(int)3840, height=(int)2160, format=(string)I420, framerate=(fraction)30/1" \
 		! nvtee ! nvvidconv \
-		! "video/x-raw(memory:NVMM), width=(int)1920, height=(int)1080, format=(string)NV12" \
+		! "video/x-raw(memory:NVMM), width=(int)3840, height=(int)2160, format=(string)NV12" \
 		! omxh265enc \
 		! h265parse \
-		! splitmuxsink location=$FILE_PATH/$DAYANDTIME-%05d.mkv max-size-time=5000000000 muxer=matroskamux
+		! mux. \
+	alsasrc device=$A_DEV \
+		! queue max-size-buffers=0 max-size-time=0 max-size-bytes=0 \
+		! audioconvert \
+		! voaacenc \
+		! mux. \
+		! splitmuxsink muxer="matroskamux name=mux max-size-time=9000000000 location=$FILE_PATH/$DAYANDTIME-%05d.mkv"
+	;;
+	-4)
+	GST_DEBUG=3 gst-launch-1.0 -e nvcamerasrc fpsRange="30 30" \
+		! "video/x-raw(memory:NVMM), width=(int)3840, height=(int)2160, format=(string)I420, framerate=(fraction)30/1" \
+		! nvtee ! nvvidconv \
+		! "video/x-raw(memory:NVMM), width=(int)3840, height=(int)2160, format=(string)NV12" \
+		! omxh265enc \
+		! h265parse \
+		! splitmuxsink location=$FILE_PATH/$DAYANDTIME-%05d.mkv max-size-time=9000000000 muxer=matroskamux
 	;;
 	-3)
 	GST_DEBUG=3 gst-launch-1.0 -e nvcamerasrc fpsRange="30 30" \
